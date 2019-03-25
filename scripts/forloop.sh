50f9a651d5fb25a153865c39607c8365bb0ad1b5
elonmusk408 <46572149+elonmusk408@users.noreply.github.com>
2019-03-25 14:33:39 +0530

    Update forloop.sh

--- scripts/forloop.sh
+++ scripts/forloop.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-#
+# display numbers from ascending and descending order
 
 
 
 for (( i=1; i<=10; i++ ))
+  do
+      echo "values from top to bottom $i"
+done
+
+for (( i=10; i>=1; i-- ))
   do
       echo "values from top to bottom $i"
 done
